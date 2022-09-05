6d5b269a61c01bcb41cd42d2876e4c3be1e64cd4
SampiniNicolas <sampini.nicolas@hotmail.com>
2022-09-05 19:07:51 -0300

    CambiosEstudiante

--- Programacion Orientada a Objetos/ElEJemploUniversal/Estudiante.cs
+++ Programacion Orientada a Objetos/ElEJemploUniversal/Estudiante.cs
@@ -8,13 +8,34 @@ namespace ElEJemploUniversal
 {
     public class Estudiante
     {
-        public string apellido;
-        public string legajo;
-        public string nombre;
-        public int notaPrimerParcial;
-        public int notaSegundoParcial;
-        public Random random;
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
 
-        
+        public Estudiante(string nombre, string apellido, string legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public int SetNotaPrimerParcial
+        {
+            set
+            {
+                if (value > 0 && value <= 10)
+                {
+                    this.notaPrimerParcial = value;
+                }
+            }
+        }
     }
 }
